c479a388b59ce45e82d44e7bb56eb6b18f57460f
Blake Hansen <hansen.blake@student.greenriver.edu>
2024-04-18 12:33:54 -0400

    Updated order forms and php

--- index.php
+++ index.php
@@ -55,30 +55,36 @@ $f3->route('GET|POST /order1', function($f3) {
     //echo '<h1>My Breakfast Menu</h1>';
 
     // If form submitted
-    if ($_SERVER['REQUEST_METHOD'] = "POST") {
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
 
-        // Get daata from post array
+        // Get data from post array
         $food = $_POST['food'];
         $meal = $_POST['meal'];
 
         // If data is valid
-        if (true){
+        if (true) {
+
+            // Add data to sessions array
             $f3->set('SESSION.food', $food);
             $f3->set('SESSION.meal', $meal);
-        }
-        echo "<p>Got here in post method</p>";
-        var_dump ($_POST);
-    } else {
-        echo "<p>Got here in the get method</p>";
-    }
 
+            // Send user to the next form
+            $f3->reroute('order2');
+        }
+        else {
+            // temp
+            echo "<p>Validation errors</p>";
+        }
+    }
     // Render a view page
     $view = new Template();
     echo $view->render('views/order1.html');
 });
 
-// order2 menu
-$f3->route('GET /order2', function() {
+// order2 menu                     make $f3 an annonoumas function
+$f3->route('GET /order2', function($f3) {
+
+    var_dump ( $f3->get('SESSION') );
     //echo '<h1>My Breakfast Menu</h1>';
 
     // Render a view page
